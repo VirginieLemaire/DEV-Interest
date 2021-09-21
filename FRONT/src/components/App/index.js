7d54334549eb5cf89a4f3a4848cf3190f433097f
AlexandreDemartin <alx.demartin@gmail.com>
2021-09-21 14:32:44 +0200

    Setup front generic component (je sais que je dois pas pousser sur la branche DEV my bad

--- FRONT/src/components/App/index.js
+++ FRONT/src/components/App/index.js
@@ -1,12 +1,20 @@
 // == Import
+import Button from '../GenericComponents/Button';
 import './app.scss';
 
 // == Composant
-const App = () => (
+const App = () => {
+  
+  const handleClick = (e) =>{
+    e.preventDefault();
+  }
+
+  return(
   <div className="app">
     <h1>Composant : App</h1>
+    <Button color styling="full" submit handleClick={handleClick} content="Se connecter" />
   </div>
 );
-
+}
 // == Export
 export default App;
