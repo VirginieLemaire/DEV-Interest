30ad515021566e18eb6f812b1aac9069c370fe9b
Frbot25 <frederic.botella@gmail.com>
2021-09-22 11:03:33 +0200

    /models/cards.js for express

---
+++ BACK/app/models/cards.js
@@ -0,0 +1,23 @@
+const client = require('../database');
+
+class Cards {
+    constructor(obj ={}) {
+        for (const proname in obj) {
+            this[proname] = obj[proname];
+        }
+    }
+    static async findAllCards(limit,skip) {
+        try {
+            
+            const {rows} = await client.query(`SELECT * FROM card LIMIT ${limit} OFFSET ${skip}`);
+           
+                return rows.map(row => new Cards(row));
+            
+                
+        }catch(error) {
+            console.log(error);
+            throw new Error(error.detail ? error.detail : error.message);
+        }
+    }
+}
+module.exports = Cards;    
