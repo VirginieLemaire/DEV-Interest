0984200c8c707ad0aa84d7ac4a6309af8959d385
Tahus <t.hassouna@hotmail.fr>
2021-09-22 13:39:37 +0200

    component dynamic

--- FRONT/src/components/App/index.js
+++ FRONT/src/components/App/index.js
@@ -1,6 +1,8 @@
-// == Import
+//Import
 import Button from '../GenericComponents/Button';
 import CardDetails from '../CardDetails';
+import cardData from '../../data';
+import technos from '../../data';
 import './app.scss';
 
 // == Composant
@@ -14,10 +16,10 @@ const App = () => {
   <div className="app">
     <h1>Composant : App</h1>
     <Button color styling="full" submit handleClick={handleClick} content="Se connecter" />
-    <CardDetails />
-    
+    <CardDetails data={cardData} data={technos} />
   </div>
 );
 }
 // == Export
 export default App;
+
--- FRONT/src/components/CardDetails/index.js
+++ FRONT/src/components/CardDetails/index.js
@@ -1,24 +1,50 @@
+import PropTypes from 'prop-types';
 import './card-details.scss';
 
-const CardDetails = ()=>{
+const CardDetails = ({ data })=>{
 
   return (
-<div className='card__container'>
-  
-    <img className='image__card'src='https://www.mentalhealthtoday.co.uk/media/37238/young-people-diversity-prime.jpg'/>
-    <h2 className='card__title'> Apprendre react en 5 minutes</h2>
-    <p className='card__pseudo'>proposé par:  </p>
-    <p className='card__date'>le 12 septembre 2021</p>
-    <p className='card__level'>Débutant</p>
-    <p className='card__language'> JS CSS</p>
-    <p className='card__category'>Apprendre</p>
-    <button className='card__button'>Vidéo</button>
-    <button className='card__bookmark'>Favoris</button>
+<div className='card'>
 
-  </div>
-  )
-  
+  <img className='card__image'src={ data[0].image } />
+  <h2 className='card__title'> { data[0].title }</h2>
+  <div className='card__langue'>{ data[0].language }</div>
+  <p className='card__article'> { data[0].description } </p>
+  <div className='card__pseudo'>Publié par : { data[0].contributor } </div>
+  <div className='card__date'> { data[0].createdAt } </div>
+  <div className='card__level'> { data[0].level } </div>
+  <div className='card__language'> { data[0].technos }</div>
+  <p className='card__category'> { data[0].category } </p>
+  <button className='card__button' href='#'> { data[0].media } </button>
+  <button className='card__bookmark' href='#'> { data[0].bookmark } </button>
+
+
+
+</div>
+)
+
 };
+
+CardDetails.propTypes = { 
+data: PropTypes.arrayOf(
+PropTypes.shape({
+  image : PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  contributor: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired,
+  level: PropTypes.string.isRequired,
+  technos: PropTypes.array.isRequired,
+  category: PropTypes.string.isRequired,
+  bookmark: PropTypes.string.isRequired,
+
+}),
+).isRequired,
+};
+
+
+
 
 export default CardDetails
  
