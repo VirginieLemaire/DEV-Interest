add78c35317927d62bd8dbd05c84043476f5b4a9
Frbot25 <frederic.botella@gmail.com>
2021-09-22 11:11:20 +0200

    /controllers/cardsControllers.js for express

---
+++ BACK/app/controllers/cardsControllers.js
@@ -0,0 +1,24 @@
+const Cards = require('../models/cards');
+
+const cardsController = {
+    findAllCards: async (req, res) => {
+        try {
+            let skip = req.query.skip;
+            let limit = 30;
+            
+            const card = await Cards.findAllCards(limit,skip);
+            if(card === "") {
+                console.log('vide');
+                res.status(200).json('Pas de contenu !');
+            }else {
+                
+                res.json(card);
+            }
+            
+        } catch(error) {
+            console.log(error);
+            res.status(500).json(error.message);
+        }
+    }
+}
+module.exports = cardsController;
