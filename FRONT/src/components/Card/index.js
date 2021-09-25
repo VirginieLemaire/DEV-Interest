d59d3c6510a3da0d1695a80c0518f86513cde3d8
romainAccoce <romainaccoce@gmail.com>
2021-09-26 01:31:40 +0200

    add empty/fill bokkmark icon toggle

--- FRONT/src/components/Card/index.js
+++ FRONT/src/components/Card/index.js
@@ -1,44 +1,62 @@
+import { useSelector } from 'react-redux';
 import { BsBookmark } from '@react-icons/all-files/bs/BsBookmark';
+import { BsBookmarkFill } from '@react-icons/all-files/bs/BsBookmarkFill'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import './card.scss';
 
 // == Composant
-const Card = ({ card }) => (
-  <div className="card">
-    <Link className="card_link" to={`/cards/${card.slug}/${card.id}`}>
-      <img className="card__image" src={card.image} alt={card.title} />
-    </Link>
-    <div className="card__buttons-group">
-      <a className="card__button media" type="button" href={card.url}>{card.type}</a>
-      <div className="card__button bookmark" type="button"><BsBookmark /></div>
-    </div>
-    <Link className="card_link" to={`/cards/${card.slug}/${card.id}`}>
-      <h2 className="card__title">{card.title}</h2>
-      <h3 className="card__website">{card.website}</h3>
-    </Link>
-    <div className="card__meta">
-      <div className="card__tags">
-        <div className="card__tags-category">
-          <aside className={`card__tags-category--item ${card.category.toLowerCase()}`}>{card.category}</aside>
+const Card = ({ card }) => {
+  const bookmarks = useSelector((state) => state.user.bookmarks);
+  const isBookmarked = bookmarks.find((bookmark) => bookmark.id === card.id)
+
+
+  return (
+    <div className="card">
+      <Link className="card_link" to={`/cards/${card.slug}/${card.id}`}>
+        <img className="card__image" src={card.image} alt={card.title} />
+      </Link>
+      <div className="card__buttons-group">
+        <a className="card__button media" type="button" href={card.url}>{card.type}</a>
+        {
+          (isBookmarked) && (
+            <div className="card__button bookmark" type="button"><BsBookmarkFill /></div>
+          )
+        }
+        {
+          (!isBookmarked) && (
+            <div className="card__button bookmark" type="button"><BsBookmark /></div>
+          )
+        }
+
+      </div>
+      <Link className="card_link" to={`/cards/${card.slug}/${card.id}`}>
+        <h2 className="card__title">{card.title}</h2>
+        <h3 className="card__website">{card.website}</h3>
+      </Link>
+      <div className="card__meta">
+        <div className="card__tags">
+          <div className="card__tags-category">
+            <aside className={`card__tags-category--item ${card.category.toLowerCase()}`}>{card.category}</aside>
+          </div>
+          <div className="card__tags-techno">
+            {
+              card.techs.map(
+                (tech) => (
+                  <aside key={`${card.id}-${tech}`} className={`card__tags-techno--item ${tech.toLowerCase()}`}>{tech}</aside>
+                ),
+              )
+            }
+          </div>
         </div>
-        <div className="card__tags-techno">
-          {
-            card.techs.map(
-              (tech) => (
-                <aside key={`${card.id}-${tech}`} className={`card__tags-techno--item ${tech.toLowerCase()}`}>{tech}</aside>
-              ),
-            )
-          }
+        <div className={`card__level ${card.level.toLowerCase()}`}>
+          <div>{card.level}</div>
         </div>
       </div>
-      <div className={`card__level ${card.level.toLowerCase()}`}>
-        <div>{card.level}</div>
-      </div>
     </div>
-  </div>
-);
+  );
+}
 
 Card.propTypes = {
   card: PropTypes.shape({
