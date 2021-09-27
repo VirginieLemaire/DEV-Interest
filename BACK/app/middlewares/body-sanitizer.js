0958874e4ec3e47c8526a4bd46cc82603a22276e
Virginie <virginielemaire@free.fr>
2021-09-27 16:06:52 +0200

    secured app against XSS attacks with sanitizer

--- BACK/app/middlewares/body-sanitizer.js
+++ BACK/app/middlewares/body-sanitizer.js
@@ -4,7 +4,7 @@ const bodySanitizer = (req, res, next) => {
     // boucler sur les clés
     for (let propName in req.body) {
         console.log('Avant :', req.body[propName]);
-        req.body[propName] = sanitizer.escape(req.body[propName]);
+        req.body[propName] = sanitizer.escape(req.body[propName]);// Escapes HTML special characters in attribute values as HTML entities
         console.log('Après :', req.body[propName]);
     };
     next();
