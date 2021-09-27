32085373aaf90ef669444b16ebe51da46b72823c
AlexandreDemartin <alx.demartin@gmail.com>
2021-09-27 17:55:31 +0200

    favicon ok

--- FRONT/src/components/TermsOfUse/index.js
+++ FRONT/src/components/TermsOfUse/index.js
@@ -1,8 +1,16 @@
+import { NavHashLink } from 'react-router-hash-link';
+
 import './terms-of-use.scss';
 
 const TermsOfUse = () => (
   <div className="terms-of-use">
     Terms of use
+    <NavHashLink
+      to="#with-hash-fragment"
+      activeClassName="selected"
+      activeStyle={{ color: 'red' }}
+    >Link to Hash Fragment
+    </NavHashLink>
   </div>
 );
 
