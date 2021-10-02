d3970026b8bf4b9ce25812300fc1a5a4d64c013b
romainAccoce <romainaccoce@gmail.com>
2021-10-03 00:12:55 +0200

    add link to card details view on slide click

--- FRONT/src/components/Slider/index.js
+++ FRONT/src/components/Slider/index.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
 import './slider.scss';
 
 
@@ -25,10 +26,12 @@ const Slider = () => {
       >
         {
           data.map((slide) => (
-            <div key={slide.id} className="slide">
+            <Link key={slide.id} to={`/cards/${slide.slug}/${slide.id}`}>
+            <div  className="slide">
               <img  className="slide__image" src={slide.image} alt=""/>
               <h2 className="slide__title">{slide.title}</h2>
             </div>
+            </Link>
           )
         )}
       </Carousel>
