b24a43e8f1308a77ed240c3ceef8f4c88c5d752f
romainAccoce <romainaccoce@gmail.com>
2021-10-13 15:12:41 +0200

    add cards display component

--- FRONT/src/components/404/index.js
+++ FRONT/src/components/404/index.js
@@ -6,67 +6,12 @@ import { useHistory } from 'react-router-dom';
 import Masonry from 'react-masonry-css';
 import Button from '../GenericComponents/Button';
 import AppLoader from '../GenericComponents/AppLoader';
-import SearchResults from '../SearchResults';
+import CardsDisplay from '../CardsDisplay';
 import { fetchCardsSearch, setCurrentSearch } from '../../action/cardsSearch';
 
 const ErrorPage = () => {
   const dispatch = useDispatch();
-  const { loading } = useSelector((state) => state.displayOptions);
   const history = useHistory();
-  
-  const fakeCards = [
-    {
-      id:1,
-      title:"Signification du Chiffre 4",
-      image:"https://www.nombresdesanges.com/wp-content/uploads/2018/09/4.jpg",
-      description:"Error",
-      category:"Autre",
-      level:"Débutant",
-      type:"article",
-      slug: '',
-      website: '',
-      url: '',
-      techs: ["Autre"],
-      lang: '',
-    
-
-    },
-    {
-      id:2,
-      title:"Diviser 0 par 0",
-      image:"https://ms3.embroideryshristi.com/3878-thickbox_default/news-outline-number-0.jpg",
-      description:"Null",
-      category:"Autre",
-      level:"Expert",
-      type:"article",
-      slug: '',
-      website: '',
-      url: '',
-      techs: ["Autre"],
-      lang: '',
-    },
-    {
-      id:3,
-      title:"Métro ligne 4",
-      image:"https://www.transilien.com/sites/transilien/files/styles/cards_bubble_crop/public/2021-06/metro_4_fc.png?h=4ff4a9b9&itok=dtFleUEC",
-      description:"Lost",
-      category:"Autre",
-      level:"Débutant",
-      type:"article",
-      slug: '',
-      website: '',
-      url: '',
-      techs: ["Autre"],
-      lang: '',
-    },
-  ];
-
-  useEffect(() => {
-    dispatch(setCurrentSearch(404));
-    dispatch(fetchCardsSearch());
-  }, []);
-
-  if (loading) return AppLoader;
 
   return (
     <div className="error-page">
@@ -94,7 +39,7 @@ const ErrorPage = () => {
               )
             }
         </Masonry> */}
-        <SearchResults />
+        <CardsDisplay />
       </div>
     </div>
   );
---
+++ FRONT/src/components/CardsDisplay/index.js
@@ -0,0 +1,92 @@
+import { useDispatch, useSelector } from 'react-redux';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Masonry from 'react-masonry-css';
+import { useLocation } from 'react-router';
+import { useEffect } from 'react';
+import { fetchCardsSearch, LoadMoreResults, setFilter } from '../../action/cardsSearch';
+
+import './cards-display.scss';
+
+import Card from '../Card';
+import AppLoader from '../GenericComponents/AppLoader';
+import Loader from '../GenericComponents/Loader';
+import SearchFilters from '../SearchFilters';
+
+const SearchResults = () => {
+  const dispatch = useDispatch();
+
+  // const useQuery = () => new URLSearchParams(useLocation().search);
+
+  // const query = useQuery();
+
+  // const keywords = query.get('keywords');
+  // const techFilter = query.get('tech');
+  // const categoryFilter = query.get('category');
+  // const levelFilter = query.get('level');
+  // const typeFilter = query.get('type');
+  // const langFilter = query.get('lang');
+
+  const keywords = "404";
+  const techFilter = "all";
+  const categoryFilter = "all";
+  const levelFilter = "all";
+  const typeFilter = "all";
+  const langFilter = "all";
+
+
+  useEffect(() => {
+    dispatch(fetchCardsSearch(
+      keywords, techFilter, categoryFilter, levelFilter, typeFilter, langFilter,
+    ));
+  }, [keywords, techFilter, categoryFilter, levelFilter, typeFilter, langFilter]);
+
+  const { cards, searchCount } = useSelector((state) => state.cardsSearch);
+  const { loading, more, darkMode } = useSelector((state) => state.displayOptions);
+
+  const breakpointsColumnsObj = {
+    default: 7,
+    2500: 6,
+    2050: 5,
+    1750: 4,
+    1400: 3,
+    1100: 2,
+    700: 1,
+  };
+
+  if (loading) return <Loader />;
+
+  return (
+    <div className="search-container">
+      <SearchFilters />
+      <div className={darkMode ? 'search-counter search-counter--dark' : 'search-counter'}> {searchCount > 0 ? `Il y a  ${searchCount} résultats` : 'Aucun résultat'}</div>
+
+      <div className="search-results">
+        <InfiniteScroll
+          dataLength={cards.length}
+          next={() => dispatch(LoadMoreResults(
+            keywords, techFilter, categoryFilter, levelFilter, typeFilter, langFilter,
+          ))}
+          hasMore={more}
+          loader={more ? null : (<AppLoader />)}
+        >
+          <Masonry
+            breakpointCols={breakpointsColumnsObj}
+            className="masonry-grid"
+            columnClassName="masonry-grid_column"
+          >
+            {
+                cards.map(
+                  (card) => (
+                    <div className="masonry-div" key={card.id}>
+                      <Card key={card.id} card={card} />
+                    </div>
+                  ),
+                )
+              }
+          </Masonry>
+        </InfiniteScroll>
+      </div>
+    </div>
+  );
+};
+export default SearchResults;
