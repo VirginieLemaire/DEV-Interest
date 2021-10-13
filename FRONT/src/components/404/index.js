863a88ca1a5d3f65711993f3ff6086234c6c13da
romainAccoce <romainaccoce@gmail.com>
2021-10-13 16:29:16 +0200

    add darkmode to 404 page

--- FRONT/src/components/404/index.js
+++ FRONT/src/components/404/index.js
@@ -1,20 +1,15 @@
-import Card from '../Card';
 import './404.scss';
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import Masonry from 'react-masonry-css';
+import { useSelector } from 'react-redux';
 import Button from '../GenericComponents/Button';
-import AppLoader from '../GenericComponents/AppLoader';
 import CardsDisplay from '../CardsDisplay';
-import { fetchCardsSearch, setCurrentSearch } from '../../action/cardsSearch';
 
 const ErrorPage = () => {
-  const dispatch = useDispatch();
   const history = useHistory();
+  const { darkMode } = useSelector((state) => state.displayOptions);
 
   return (
-    <div className="error-page">
+    <div className={darkMode ? "error-page error-page--dark" : "error-page"}  >
       <h1 className="error-page__title">Oups, cette page n'existe pas ...</h1>
       <Button
         color
@@ -24,21 +19,6 @@ const ErrorPage = () => {
       />
       <p className="error-page__text">Nous pouvons tout de même de proposer quelques lectures sur la 404 ...</p>
       <div className="error-page__cards">
-        {/* <Masonry
-          breakpointCols={3}
-          className="masonry-grid"
-          columnClassName="masonry-grid_column"
-        >
-          {
-              fakeCards.map(
-                (card) => (
-                  <div className="masonry-div" key={card.id}>
-                    <Card key={card.id} card={card} />
-                  </div>
-                ),
-              )
-            }
-        </Masonry> */}
         <CardsDisplay keyword={'404'}/>
       </div>
     </div>
