f459d371e57ff72e680bec4d15e565740b3ef5a5
romainAccoce <romainaccoce@gmail.com>
2021-10-14 10:11:06 +0200

    add turn arrow to CardTeamBack

--- FRONT/src/components/CardTeamBack/index.js
+++ FRONT/src/components/CardTeamBack/index.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 
 import PropTypes from 'prop-types';
 import { GiTechnoHeart } from '@react-icons/all-files/gi/GiTechnoHeart';
+import { GiReturnArrow } from '@react-icons/all-files/gi/GiReturnArrow';
 
 import './card-team-back.scss';
 
@@ -19,10 +20,18 @@ const CardTeamBack = ({ card }) => {
     <div className={darkMode ? 'card-team card-team--dark' : 'card-team'}>
       <div className="card-team__back">
         {
-          card.letter && <div className="card-team__back__letter">{card.letter}</div>
+          card.letter && 
+            <div className="card-team__back__letter">
+              {card.letter}
+              <div className="card-team__back__letter__arrow"><GiReturnArrow /></div>
+            </div>
         }
         {
-          !card.letter && <div className="card-team__back__heart"><GiTechnoHeart /></div>
+          !card.letter && 
+            <div className="card-team__back__heart">
+              <GiTechnoHeart />
+              <div className="card-team__back__heart__arrow"><GiReturnArrow /></div>
+            </div>
         }
       </div>
     </div>
